4c35c21b2fd744517d9be47e586072402b90a1d1
Alex Findlay <alex.k.f@icloud.com>
2019-05-23 13:22:16 +1000

    Added ViewController
    Added wkwebview to ViewController

--- happ/ViewController.swift
+++ happ/ViewController.swift
@@ -5,23 +5,26 @@
 //  Created by Alex Findlay on 22/5/19.
 //  Copyright © 2019 Alex Findlay. All rights reserved.
 //
-
 import Cocoa
+import WebKit
 
 class ViewController: NSViewController {
-
-    override func viewDidLoad() {
+    
+    var loginWebView: WKWebView!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-
+        self.loginWebView = WKWebView(frame: self.view.frame)
+        self.view.addSubview(loginWebView)
+        let urlReq = URLRequest(url: URL(string: "http://homeassistant:8123/")!)
+        self.loginWebView.load(urlReq)
         // Do any additional setup after loading the view.
     }
-
+    
     override var representedObject: Any? {
         didSet {
-        // Update the view, if already loaded.
+            // Update the view, if already loaded.
         }
     }
-
-
 }
-
