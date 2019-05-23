5448908c364465ba3fa8647409ebb9edb06357bf
Alex Findlay <alex.k.f@icloud.com>
2019-05-23 13:20:18 +1000

    Project Created

---
+++ happ/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  happ
+//
+//  Created by Alex Findlay on 22/5/19.
+//  Copyright © 2019 Alex Findlay. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
